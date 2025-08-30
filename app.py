c0e00cb6001b4e022ca1ab1c81a3eaa30570102a
t-t34m <teamtanapatteam@gmail.com>
2025-08-30 14:22:50 +0700

    add app.middleware

--- app.py
+++ app.py
@@ -9,9 +9,18 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import joblib
 import xgboost
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # for testing, allow all; later restrict to your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mediapipe setup
 mp_pose = mp.solutions.pose
